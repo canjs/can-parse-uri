aac864f43f74a8dadeda951f806d3e353f2e82f9
Justin Meyer <justinbmeyer@gmail.com>
2018-05-11 13:18:47 -0500

    1.0.2

---
+++ dist/amd/can-parse-uri.js
@@ -0,0 +1,23 @@
+/*can-parse-uri@1.0.1#can-parse-uri*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-namespace'
+], function (require, exports, module) {
+    var namespace = require('can-namespace');
+    module.exports = namespace.parseURI = function (url) {
+        var m = String(url).replace(/^\s+|\s+$/g, '').match(/^([^:\/?#]+:)?(\/\/(?:[^:@]*(?::[^:@]*)?@)?(([^:\/?#]*)(?::(\d*))?))?([^?#]*)(\?[^#]*)?(#[\s\S]*)?/);
+        return m ? {
+            href: m[0] || '',
+            protocol: m[1] || '',
+            authority: m[2] || '',
+            host: m[3] || '',
+            hostname: m[4] || '',
+            port: m[5] || '',
+            pathname: m[6] || '',
+            search: m[7] || '',
+            hash: m[8] || ''
+        } : null;
+    };
+});
