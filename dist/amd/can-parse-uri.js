c359982584227d328903eee03a37b28335050c29
Chasen Le Hara <chasen@chasenlehara.com>
2018-07-03 22:56:33 -0700

    1.2.0

---
+++ dist/amd/can-parse-uri.js
@@ -0,0 +1,24 @@
+/*can-parse-uri@1.1.0#can-parse-uri*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-namespace'
+], function (require, exports, module) {
+    'use strict';
+    var namespace = require('can-namespace');
+    module.exports = namespace.parseURI = function (url) {
+        var m = String(url).replace(/^\s+|\s+$/g, '').match(/^([^:\/?#]+:)?(\/\/(?:[^:@]*(?::[^:@]*)?@)?(([^:\/?#]*)(?::(\d*))?))?([^?#]*)(\?[^#]*)?(#[\s\S]*)?/);
+        return m ? {
+            href: m[0] || '',
+            protocol: m[1] || '',
+            authority: m[2] || '',
+            host: m[3] || '',
+            hostname: m[4] || '',
+            port: m[5] || '',
+            pathname: m[6] || '',
+            search: m[7] || '',
+            hash: m[8] || ''
+        } : null;
+    };
+});
